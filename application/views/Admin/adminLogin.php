f69a1b4bd6ce29e558e4b2d8c2701094bdbf92c9
NitaiKOIRI463 <82794446+NitaiKOIRI463@users.noreply.github.com>
2022-05-20 12:21:23 +0530

    ui3

--- application/views/Admin/adminLogin.php
+++ application/views/Admin/adminLogin.php
@@ -73,7 +73,7 @@
             
                                         <div class="mb-3">
                                             <label for="username" class="form-label">Username</label>
-                                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter mobile number" required>
+                                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter username" required>
                                         </div>
                 
                                         <div class="mb-3">
